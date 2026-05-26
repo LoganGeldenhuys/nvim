260da0dcb6377c495d40fb671e15c7a994db4a37
LoganGeldenhuys <16024083+LoganGeldenhuys@users.noreply.github.com>
2026-05-26 16:03:03 -0500

    fix: tmux and vim navigation

--- lua/plugins/tmux-navigator.lua
+++ lua/plugins/tmux-navigator.lua
@@ -1,4 +1,11 @@
 return {
   "christoomey/vim-tmux-navigator",
   lazy = false,
+  keys = {
+    { "<C-h>",  "<cmd>TmuxNavigateLeft<cr>",     mode = { "n", "t" }, desc = "Navigate Left (Tmux)" },
+    { "<C-j>",  "<cmd>TmuxNavigateDown<cr>",     mode = { "n", "t" }, desc = "Navigate Down (Tmux)" },
+    { "<C-k>",  "<cmd>TmuxNavigateUp<cr>",       mode = { "n", "t" }, desc = "Navigate Up (Tmux)" },
+    { "<C-l>",  "<cmd>TmuxNavigateRight<cr>",    mode = { "n", "t" }, desc = "Navigate Right (Tmux)" },
+    { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", mode = { "n", "t" }, desc = "Navigate Previous (Tmux)" },
+  },
 }
